8a5f46b612b586312f7b7bc6bf4e940bf85369ff
Tiền Minh Vy <tienminhvydev@gmail.com>
2023-11-17 16:29:13 +0700

    Update abc-implementation.js

--- assets/js/abc-implementation.js
+++ assets/js/abc-implementation.js
@@ -1,6 +1,46 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   let abcCodeBlocks = document.querySelectorAll('.language-abc');
 
+  abcCodeBlocks.forEach(abcE => {
+    const abcString = abcE.innerText;
+    const musicSheet = document.createElement("div");
+    const visualObj = ABCJS.renderAbc(musicSheet, abcString);
+    const audioPlayer = document.createElement("div");
+    abcE.parentElement.insertAdjacentElement("afterend", musicSheet);
+    musicSheet.insertAdjacentElement("afterend", audioPlayer);
+    if (ABCJS.synth.supportsAudio()) {
+      let controlOptions = {
+          displayRestart: true,
+          displayPlay: true,
+          displayProgress: true,
+          displayClock: true,
+          displayWarp: true,
+          displayLoop: true
+      };
+
+      const synthControl = new ABCJS.synth.SynthController();
+      synthControl.load(audioPlayer, CursorControl, controlOptions);
+      synthControl.disable(true);
+      const midiBuffer = new ABCJS.synth.CreateSynth();
+      midiBuffer.init({
+          visualObj: visualObj[0],
+          options: {
+              
+          }
+
+      }).then(function () {
+          synthControl.setTune(visualObj[0], true).then(function (response) {
+          audioPlayer.querySelector(".abcjs-inline-audio").classList.remove("disabled");
+          })
+      });
+    } else {
+      // audio not supported
+    }
+    
+    abcE.remove();
+    musicSheet.style.overflow = "visible";
+  })
+
   function CursorControl(rootSelector) {
     var self = this;
 
@@ -13,7 +53,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
       self.onStart = function() {
           // This is called when the timer starts so we know the svg has been drawn by now.
           // Create the cursor and add it to the sheet music's svg.
-          var svg = document.querySelector(self.rootSelector + " svg");
+          var svg = self.rootSelector.querySelector("svg");
           self.cursor = document.createElementNS("http://www.w3.org/2000/svg", "line");
           self.cursor.setAttribute("class", "abcjs-cursor");
           self.cursor.setAttributeNS(null, 'x1', 0);
@@ -71,43 +111,4 @@ document.addEventListener("DOMContentLoaded", (event) => {
       
       };
   }
-  
-  abcCodeBlocks.forEach(abcE => {
-    const abcString = abcE.innerText;
-    const musicSheet = document.createElement("div");
-    const visualObj = ABCJS.renderAbc(musicSheet, abcString);
-    const audioPlayer = document.createElement("div");
-    abcE.parentElement.insertAdjacentElement("afterend", musicSheet);
-    musicSheet.insertAdjacentElement("afterend", audioPlayer);
-    if (ABCJS.synth.supportsAudio()) {
-      let controlOptions = {
-          displayRestart: true,
-          displayPlay: true,
-          displayProgress: true,
-          displayClock: true,
-          displayWarp: true,
-          displayLoop: true
-      };
-
-      const synthControl = new ABCJS.synth.SynthController();
-      synthControl.load(audioPlayer, CursorControl, controlOptions);
-      synthControl.disable(true);
-      const midiBuffer = new ABCJS.synth.CreateSynth();
-      midiBuffer.init({
-          visualObj: visualObj[0],
-          options: {
-              
-          }
-
-      }).then(function () {
-          synthControl.setTune(visualObj[0], true).then(function (response) {
-          audioPlayer.querySelector(".abcjs-inline-audio").classList.remove("disabled");
-          })
-      });
-    } else {
-      // audio not supported
-    }
-    abcE.remove();
-    musicSheet.style.overflow = "visible";
-  })
 });
